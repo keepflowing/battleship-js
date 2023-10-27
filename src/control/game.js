a78b73a3bb6ae04c333f83bf6155711e38769214
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-10-27 15:01:54 -0500

    Allow player to place their ships

--- src/control/game.js
+++ src/control/game.js
@@ -6,17 +6,18 @@ import loadBoards from '../ui/loadBoards';
 const init = () => {
   const menuPlayer = new Player('start');
   const menu = loadMenu(menuPlayer);
-  document.body.appendChild(menu);
+  document.body.appendChild(menu[0]);
   const startBtn = document.getElementById('startButton');
   startBtn.addEventListener('click', () => {
-    start();
+    start(menu[1]);
   });
 };
 
-const start = () => {
+const start = (squares) => {
   const p1 = new Player('p1');
   const p2 = new Player('p2', true);
-  p1.gameboard.randomPlaceShips();
+  if (squares) p1.gameboard.squares = squares;
+  else p1.gameboard.randomPlaceShips();
   p2.gameboard.randomPlaceShips();
 
   p2.gameboard.placeShip('G5', new Ship(2, 4, true));
--- src/ui/loadMenu.js
+++ src/ui/loadMenu.js
@@ -1,24 +1,20 @@
 import loadBoards from './loadBoards';
 import Ship from '../class/Ship';
 
-// module
-const loadMenu = (p) => {
-  p.gameboard.init();
-  const board = loadBoards(p);
-  const div = document.createElement('div');
-  div.id = 'startMenu';
-  div.appendChild(board);
-  const testSquare = document.createElement('div');
-  testSquare.className = 'ship square';
-  testSquare.setAttribute('draggable', true);
-  testSquare.addEventListener('dragstart', (e) => {
+const initDragnDrop = (square, p) => {
+  square.addEventListener('dragstart', (e) => {
+    let data = JSON.parse(square.getAttribute('data'));
     const squares = document.getElementsByClassName('square');
     for (let i = 0; i < squares.length; i++) {
       squares[i].addEventListener('dragover', (e) => {
         e.preventDefault();
       });
       squares[i].addEventListener('drop', (e) => {
-        p.gameboard.placeShip(squares[i].id, new Ship(1, 5, false));
+        if (data) {
+          p.gameboard.placeShip(squares[i].id,
+              new Ship(data[0], data[1], data[2]));
+        }
+        data = null;
         for (const square in p.gameboard.squares) {
           if (p.gameboard.squares[square].hasShip) {
             document.getElementById(square).classList.add('ship');
@@ -27,13 +23,43 @@ const loadMenu = (p) => {
       });
     }
   });
-  div.appendChild(testSquare);
+};
+
+const createShipPlacers = (p) => {
+  const ships = [[1, 5], [2, 4], [3, 3], [4, 3], [5, 2]];
+  const placers = [];
+  for (let i = 0; i < ships.length; i++) {
+    const placer = document.createElement('div');
+
+    placer.className = 'ship square';
+    placer.innerText = ships[i][1];
+    placer.setAttribute('draggable', true);
+    placer.setAttribute('data', `[${ships[i][0]}, ${ships[i][1]}, false]`);
+    // add eventlisteners for dragging & dropping
+    initDragnDrop(placer, p);
+    placers.push(placer);
+  }
+  return placers;
+};
+
+// module
+const loadMenu = (p) => {
+  p.gameboard.init();
+  const board = loadBoards(p);
+  const div = document.createElement('div');
+  div.id = 'startMenu';
+  div.appendChild(board);
+  const shipPlacers = createShipPlacers(p);
+
+  for (let i = 0; i < shipPlacers.length; i++) {
+    div.appendChild(shipPlacers[i]);
+  }
   const button = document.createElement('button');
   button.id = 'startButton';
   button.innerText = 'Start';
   div.appendChild(button);
 
-  return div;
+  return [div, p.gameboard.squares];
 };
 
 export default loadMenu;
